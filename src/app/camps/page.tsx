c43d79f34745aaff081c762565ac00b338041956
behark <beharkabashi22@gmail.com>
2026-01-27 23:54:16 +0100

    Fix JSX syntax error in camps page

--- src/app/camps/page.tsx
+++ src/app/camps/page.tsx
@@ -129,7 +129,6 @@ export default async function CampsPage() {
                       </div>
                     </>
                   )}
-                  </div>
                 </div>
               </CardContent>
             </Card>
